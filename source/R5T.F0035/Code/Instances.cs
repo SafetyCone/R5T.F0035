3a3d16449d1586b433d152c068d3c43e86033a63
davidcoats <david@getsafetycone.com>
2024-08-27 22:53:18 -0700

    Interim commit.

--- source/R5T.F0035/Code/Instances.cs
+++ source/R5T.F0035/Code/Instances.cs
@@ -5,7 +5,7 @@ namespace R5T.F0035
 {
     public static class Instances
     {
-        public static ILoggingBuilderOperator LoggingBuilderOperator { get; } = F0035.LoggingBuilderOperator.Instance;
-        public static IServicesOperator ServicesOperator { get; } = F0035.ServicesOperator.Instance;
+        public static ILoggingBuilderOperator LoggingBuilderOperator => F0035.LoggingBuilderOperator.Instance;
+        public static IServicesOperator ServicesOperator => F0035.ServicesOperator.Instance;
     }
 }
